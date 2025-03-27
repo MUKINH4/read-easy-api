3e6308c8de3692e21f477f5ccd1b7e1f9daf3334
MUKINH4 <samuelmaragato3@gmail.com>
2025-03-27 16:46:23 -0300

    added Logger

--- src/main/java/br/com/fiap/read_easy_api/controller/GenreController.java
+++ src/main/java/br/com/fiap/read_easy_api/controller/GenreController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import br.com.fiap.read_easy_api.repository.GenreRepository;
 
 @RestController
 @RequestMapping("/genres")
+@CrossOrigin(origins = "*")
 public class GenreController {
 
     // @Autowired
